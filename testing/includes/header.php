abc7c094332b2e3eacb4d03bee452354c1166a1d
Mrbigelephant1 <mrbigelephant0@gmail.com>
2026-03-23 12:50:29 +0000

    Add files via upload

---
+++ testing/includes/header.php
@@ -0,0 +1,40 @@
+<?php
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>The Mat Works</title>
+    <link href="assets/main.css" rel="stylesheet">
+ 
+    
+    <!-- The code for the header -->
+
+  <header>
+
+    <div class="logo">The Mat Works</div>
+
+    <nav>
+        <ul class="menu">
+          
+            <li><a href="index.php">Home</a></li>
+            <li><a href="events.php">Events</a></li>
+            <li><a href="info.php">Info</a></li>
+            <li><a href="priv_hire.php">Private Hire</a></li>
+            <li><a href="tickets.php">Tickets</a></li>
+
+
+        </ul>
+    </nav>
+
+  </header>
+
+
+ 
+  </head>
+  <body>
